a4e5bb2bca039b9c90f85fc5a851d2cf7326bdef
root <root@pi.scott.net>
2014-07-18 20:32:39 -0500

    Added comment to boot start script to clarify relationship to cron schedule

--- photo_frame_boot_start.sh
+++ photo_frame_boot_start.sh
@@ -2,6 +2,9 @@
 # This only runs at boot time and decides whether the photo frame should be started or not
 # If the hour of the day is 7:00am or later AND before 10:00pm then start
 
+# Note that the times here should correspond with the times that cron is starting 
+# and stopping the photo frame. 
+
 HOUR="$(date +%H)"
 
 if [ $HOUR -gt "06" ] && [ $HOUR -lt "22" ]; then
