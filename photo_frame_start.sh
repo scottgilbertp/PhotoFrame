0d6cf225c6824996cb84144aca605359c2cce247
root <root@pi.scott.net>
2014-07-18 20:20:49 -0500

    Clean up comments

--- photo_frame_start.sh
+++ photo_frame_start.sh
@@ -3,14 +3,12 @@
 FIND='/usr/bin/find'
 EXCLUDESFILE='/root/photo_frame_excludes.txt'
 
-
 # cd to the base dir of the images so the relative path is shorter
 cd /mnt/wizhome/scottg/Photos/
 
 # Set "field separater" to end of line to allow spaces and other special chars in filepaths
 IFS=$(echo -en "\n\b")
 NEWL=$(echo -en "\n\b")
-
 
 # Note: keep the total number of images select below a few thousand to keep from exceeding max argument length
 #       If one is displaying 2 images per minute, then only 2880 images will be displayed in 24 hours.
@@ -28,7 +26,7 @@ done < $EXCLUDESFILE
 FINDCMD="$FIND ./ $EXCLUDES -iname '*.j*g' -print"
 IMAGES="$(eval $FINDCMD |/usr/bin/sort -R|/usr/bin/head -n 1000)"
 
-# Include 300 "good" pictures (ie: have an 'a' appended to filename indicating they have been edited)
+# Include 300 "good" pictures (ie: have an 'a' appended to filename suggesting they have been edited)
 FINDCMD="$FIND ./ $EXCLUDES -iname '*a.j*g' -print"
 IMAGES="${IMAGES}${NEWL}$(eval $FINDCMD |/usr/bin/sort -R|/usr/bin/head -n 500)"
 
@@ -36,7 +34,7 @@ IMAGES="${IMAGES}${NEWL}$(eval $FINDCMD |/usr/bin/sort -R|/usr/bin/head -n 500)"
 FINDCMD="$FIND ./ $EXCLUDES -iname '*.j*g' -mtime -180 -print"
 IMAGES="${IMAGES}${NEWL}$(eval $FINDCMD |/usr/bin/sort -R|/usr/bin/head -n 500)"
 
-# Include 200 "most most recent" pictures (from the last 30 days)
+# Include (up to) 200 "most most recent" pictures (from the last 10 days)
 FINDCMD="$FIND ./ $EXCLUDES -iname '*.j*g' -mtime -10 -print"
 IMAGES="${IMAGES}${NEWL}$(eval $FINDCMD |/usr/bin/sort -R|/usr/bin/head -n 200)"
 
