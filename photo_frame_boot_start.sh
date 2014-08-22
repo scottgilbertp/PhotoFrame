05c963b3cf27eb43eceabea93c44e085a9cebc98
Scott Gilbert <scott.gilbert@bum.org>
2014-08-21 20:35:58 -0500

    Updated photo_frame_boot_start.sh to use hour AND minutes instead of just hour

--- photo_frame_boot_start.sh
+++ photo_frame_boot_start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # This only runs at boot time and decides whether the photo frame should be started or not
-# If the hour of the day is 7:00am or later AND before 10:00pm then start
+# If the time of the day is 6:30am or later AND before 10:00pm then start
 
 # Note that the times here should correspond with the times that cron is starting 
 # and stopping the photo frame. 
 
-HOUR="$(date +%H)"
+TIME=$(date +%H%M)
 
-if [ $HOUR -gt "06" ] && [ $HOUR -lt "22" ]; then
-  echo "We rebooted! Hour is $HOUR, so photo frame is starting..."
+if [ $TIME -gt 0630 ] && [ $TIME -lt 2200 ]; then
+  echo "We rebooted! Time is $TIME, so photo frame is starting..."
   /root/photo_frame/photo_frame_start.sh 2>&1 >> /var/log/photo_frame.log
 else
-  echo "We rebooted! Hour is $HOUR, so photo frame is NOT starting..."
+  echo "We rebooted! Time is $TIME, so photo frame is NOT starting..."
 fi
