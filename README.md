ec13dfbe06869f384034d54d93e21aaf6c1b4004
Scott Gilbert <scott.gilbert@bum.org>
2018-12-27 12:20:48 -0600

    Major overhaul to use a config file instead of hard-coding and eliminate multiple start scripts

--- README.md
+++ README.md
@@ -8,18 +8,26 @@ Digital Photo Frame for Raspberry Pi
 - gawk - gnu awk processor (plain awk does not work)
 - nginx - webserver for display of today's list of images (optional)
 
+### Installation
+ 1. Install git and dependencies:  `apt install git fbi gawk nginx`
+ 1. Clone this repo: `git clone https://github.com/scottgilbert/PhotoFrame.git`
+ 1. Make your photos accessible on the pi's filesystem (by copying, or NFS mount, or USB drive, or whatever)
+ 1. Edit the photo_frame.conf file to meet your needs.  In particular, you will probably need to adjust `PHOTODIR`
+ 1. You should now be able to execute photo_frame_start.sh to test.
+ 1. Once verified, create cronjobs to start/stop the photo_frame. (sample provided in `crontab` file)
+    Note that if you change the start/stop timing, you will also need to modify the photo_frame_boot_start.sh script
+
 ### Files:
 - crontab - example crontab implementation  
-- photo_frame_start-*.sh  - scripts to turn on display, select images and start displaying them. Variety of scripts with different selection criteria
-- photo_frame_start.sh  - symlink to select which of the photo_frame_start-*.sh scripts to currently use (usually executed by a cron job)
+- photo_frame.conf - config file 
+- photo_frame_start.sh  - script to turn on display, select images and start displaying them. (usually executed by a cron job)
 - photo_frame_stop.sh - script to stop displaying images and turn off display (usually executed by a cron job) 
 - photo_frame_boot_start.sh - script to run at boot time which considers the current time and decides whether or not to run photo_frame_start.sh  (executed by cron "@reboot" job or a system "init" mechanism)
 - photo_frame_excludes.txt - list of filepath globs to never show (may include comments preceeded with a "#")  
-- photo_frame_phone_photos_only_start.sh  - script to turn on display, select images and start displaying them - but limited to phone photos 
 
 ### Notes:
-There are different versions of the photo_frame_start.sh start script with different photo selection criteria.  Symlink photo_frame_start.sh to whichever version you wish to use.  Or create a cron job to change the symlink for different photo selection criteria on different days!
+photo_frame_start.sh will accept a single parameter of a config file.  If no config file is specified, then the default (photo_frame.conf) is used.  Multiple config files could be created to provide different photo selections on different days.
 
-The "*_start.sh" scripts select a list of photos and displays them.  It also generates an html version of the list of photos, suitable for display by any webserver. The default location to write this file is /usr/share/nginx/www/index.html. It also produces a simple text list of photos, written to /var/log/photo_frame_image_list-$(date +%d).log. Note that this file contains the "day of month", so only about one month's of log files are kept - with older ones being overwritten by newer ones.
+The photo_frame_start.sh script selects a list of photos and displays them.  It also generates an html version of the list of photos, suitable for display by any webserver, and also a simple text list Note that,by default, this file contains the "day of month", so only one month's of log files are kept - with older ones being overwritten by newer ones.
 
-There are a lot of things that are "hard coded", which really should be defined in a config file of some sort. Maybe someday, I'll tidy this up.  See the BUGS.txt file for more details.
+There are still a few things that are "hard coded". See the BUGS.txt file for other defects and limitations..
--- photo_frame_start.sh
+++ photo_frame_start.sh
@@ -1,1 +1,140 @@
-photo_frame_start-more_anniversary.sh
+#!/bin/bash
+
+# MYDIR is the directory of this script.
+# we assume that other files are in this same dir.
+# (readlink is used to expand a possible relative path to an
+#  absolute path)
+MYDIR="$(readlink -f ${0%/*})"
+
+# If a config file is specified, use it, otherwise default
+if [[ ! -z $1 ]]; then
+  CONF_FILE="$1"
+else
+  CONF_FILE="${MYDIR}/photo_frame.conf"
+fi
+
+if [[ -r $CONF_FILE ]]; then
+  . $CONF_FILE
+else
+  echo "ERROR: Cannot find config file: $CONF_FILE"  
+  exit 77
+fi
+
+[[ $DEBUG -eq 1 ]] && echo "Using settings from config file: $CONF_FILE"
+
+# Set "field separater" to end of line to allow spaces and other special chars
+# in filepaths
+IFS=$'\n\b'
+NEWL=$'\n'
+
+# Build "find" parameters incorporating the patterns from the excludes file
+# (skipping comments and blank lines)
+EXCLUDES=""
+while read line; do
+  # strip off any stray trailing blanks
+  line="$(echo "$line" | sed 's/ *$//')"
+
+  if [[ $line != \#*  &&  ! "$line" == "" ]] ; then
+    EXCLUDES="$EXCLUDES ! -path '${line}'"
+  fi
+done < $EXCLUDESFILE
+
+[[ $DEBUG -eq 1 ]] && echo "Excludes: $EXCLUDES"
+
+# cd to the base dir of the images to minimize the relative path length
+cd $PHOTODIR
+IMAGES=''
+
+#  Include NUM_PICS_GOOD "good" pictures (ie: have an 'a' appended to filename 
+#  suggesting they have been edited)
+if [[ $NUM_PICS_GOOD -gt 0 ]]; then
+  FINDCMD="find ./ $EXCLUDES ! -path '*Jaques*' -iname '*a.j*g' -print"
+  IMAGES="$(eval $FINDCMD | shuf -n $NUM_PICS_GOOD)"
+fi
+
+[[ $DEBUG -eq 1 ]] && echo "Num of photos selected after good: $(echo "$IMAGES"|wc -l)"
+
+# Include (up to) NUM_PICS_RECENT "recent" pictures 
+# ("recent" means within the last NUM_PICS_RECENT_DAYS days)
+if [[ $NUM_PICS_RECENT -gt 0 ]]; then
+  FINDCMD="find ./ $EXCLUDES -iname '*.j*g' -mtime -${NUM_PICS_RECENT_DAYS} -print"
+  IMAGES="${IMAGES}${NEWL}$(eval $FINDCMD | shuf -n $NUM_PICS_RECENT)"
+fi
+
+[[ $DEBUG -eq 1 ]] && echo "Num of photos selected after recent: $(echo "$IMAGES"|wc -l)"
+
+# Include (up to) NUM_PICS_ANNIV pictures from "same day of the year (+/- 1 day)"
+# (match either dates with '-' separaters OR no separators and trailing underscore)
+if [[ $NUM_PICS_ANNIV -gt 0 ]]; then
+  DATES="\( -path '*-$(date +%m-%d)*' \
+        -or -path '*-$(date --date=yesterday +%m-%d)*' \
+        -or -path '*-$(date --date=tomorrow +%m-%d)*' \
+        -or -path '*$(date +%m%d)_*' \
+        -or -path '*$(date --date=yesterday +%m%d)_*' \
+        -or -path '*$(date --date=tomorrow +%m%d)_*' \)"
+
+  FINDCMD="find ./ $EXCLUDES -iname '*.j*g' ${DATES} -print"
+  IMAGES="${IMAGES}${NEWL}$(eval $FINDCMD | shuf -n $NUM_PICS_ANNIV)"
+fi
+
+[[ $DEBUG -eq 1 ]] && echo "Num of photos selected after anniv: $(echo "$IMAGES"|wc -l)"
+
+# Remove duplicates
+IMAGES=$(echo "$IMAGES" | sort | uniq)
+
+# Include enough random pictures to reach target number of pics (plus 10% to
+# compensate for removal of dups)
+[[ $DEBUG -eq 1 ]] && echo "Num of photos after removal of dups: $(echo "$IMAGES"|wc -l)"
+IMGCOUNT=$(echo "$IMAGES" | wc -l) 
+if [[ $IMGCOUNT -lt $TOTALPICS ]] ; then
+  RANDOMPICS=$(($TOTALPICS - $IMGCOUNT))
+  RANDOMPICS=$(($RANDOMPICS + $RANDOMPICS/10 + 1))
+  [[ $DEBUG -eq 1 ]] && echo "Num of random pics to add: ${RANDOMPICS}"
+  FINDCMD="find ./ $EXCLUDES -iname '*.j*g' -print"
+  IMAGES="${IMAGES}${NEWL}$(eval $FINDCMD | shuf -n $RANDOMPICS)"
+fi
+
+[[ $DEBUG -eq 1 ]] && echo "Num of pics before removal of dups: \
+  $(echo "$IMAGES" | wc -l)"
+
+# Remove duplicate listings and randomize image list
+IMAGES=$(echo "$IMAGES" | sort | uniq | shuf -n $TOTALPICS)
+[[ $DEBUG -eq 1 ]] && echo "Final number of pics:  $(echo "$IMAGES" | wc -l)" 
+[[ $EXIT_AFTER_SELECT -eq 1 ]] && exit 50
+
+# Log IMAGES list to file:
+[[ ! -z $PHOTO_LIST_TEXT ]] && echo "$IMAGES" > "$PHOTO_LIST_TEXT"
+
+# Generate web page list of today's images
+# note: Assumes that images will be displayed for an average of TOTAL_TIME_PER_PHOTO 
+#       seconds  each. This time includes: intentional display time, load time, 
+#       scaling time, and blend time.
+if [[ ! -z $PHOTO_LIST_HTML ]] ; then
+  echo "<h2> $(date +'%A %F') </h2> <br>" > "$PHOTO_LIST_HTML"
+  echo "$IMAGES" | sed 's/^\.\/\(.*\)$/<a href="Photos\/\1">\1<\/a><br>/' \
+    | nl \
+    | gawk "{print strftime(\"%H:%M - \",systime() + (\$1 * $TOTAL_TIME_PER_PHOTO) ),\$0 ; }" \
+    >> "$PHOTO_LIST_HTML"
+fi
+
+# For some reason, the framebuffer doesn't seem to always activate.
+# switching to virt console 2 seems to fix it. Someday I want to understand this. 
+chvt 2 
+
+# turn on display
+/usr/bin/tvservice -p
+# previously, I had been setting the depth.  I don't think this is needed anymore.
+#/bin/fbset -depth 16
+
+# fbi parms:
+#  -T 1   = display on first console
+#  -a     = autoscale images to display size
+#  -t     = display time each image 
+#  -u     = randomize order of images
+#  -noverbose = do not display status info at bottom of screen
+#  -f 'font' = specify font to use for status info
+#  -readahead = read ahead images into cache (pre-fetches next image 
+#               immediately after showing current image)
+#  -blend = image blend time in milliseconds
+
+fbi -T 1 -a -t $PHOTO_DISPLAY_TIME -f $TITLE_FONT -readahead -blend $BLEND_TIME $IMAGES
